0e4c6105a84cd1959d62bba425144496fcb82602
Ryan Kelley <rdkelley@users.noreply.github.com>
2020-05-21 20:42:34 -0700

    Finish auth0Provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
+import { auth0Context } from "./contexts/auth0Context";
 
 function App() {
-  return <div className="App"></div>;
+  const { isLoading, user, loginWithRedirect, logout } = useContext(
+    auth0Context
+  );
+
+  return (
+    <div className="App">
+      <div>
+        {!isLoading && !user && (
+          <>
+            <button onClick={loginWithRedirect}>Login</button>
+          </>
+        )}
+        {!isLoading && user && (
+          <>
+            <h1>You are logged in!</h1>
+            <p>Hello {user.name}</p>
+
+            {user.picture && <img src={user.picture} alt="My Avatar" />}
+
+            <div>
+              <button
+                onClick={() => logout({ returnTo: window.location.origin })}
+                className="button is-small is-dark"
+              >
+                Logout
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import { Auth0Provider } from "./contexts/auth0Context";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider>
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
